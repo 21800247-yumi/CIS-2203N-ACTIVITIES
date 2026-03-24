42db941c5877c21f54e62ba3790e1d2b3348297a
nkmag <2180024@usc.edu.ph>
2026-03-24 17:38:25 +0800

    java code

--- app/src/main/java/com/example/exercise1_part2/MainActivity.java
+++ app/src/main/java/com/example/exercise1_part2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.exercise1_part2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView counterDisplay;
+    private Button incrementButton;
+    private int mCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        counterDisplay = findViewById(R.id.counter);
+        incrementButton = findViewById(R.id.incrementButton);
+
+        if (savedInstanceState != null) {
+            mCounter = savedInstanceState.getInt("COUNT_KEY");
+        }
+
+        counterDisplay.setText(String.valueOf(mCounter));
+
+        incrementButton.setOnClickListener(v -> {
+            mCounter++;
+            counterDisplay.setText(String.valueOf(mCounter));
+        });
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("COUNT_KEY", mCounter);
+    }
+
 }
