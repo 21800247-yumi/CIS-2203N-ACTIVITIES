3d329c1a7dd5a61473526ad7c26e46f6940012bf
nkmag <2180024@usc.edu.ph>
2026-03-24 15:49:29 +0800

    text change

--- app/src/main/java/com/example/exercise1_part1/MainActivity.java
+++ app/src/main/java/com/example/exercise1_part1/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.exercise1_part1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("ID: 21800247");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        final TextView text = findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(R.string.department_chairname);
+            }
         });
     }
 }
